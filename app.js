4f029ad409c5cab2214f59be593e396a5a61f520
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-09-26 11:24:39 -0400

    Connect to MongoDB

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
 
+// App
 const app = express();
 
-require('dotenv').config();
+dotenv.config()
 
-app.get("/", (req, res) => {
-  res.send("hello from node");
-} );
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI,
+  {useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  },
+  () => console.log('connected to DB!'));
 
+mongoose.connection.on('error',
+err => {
+  console.log(`DB connection error: ${err.message}`)
+});
+
+// Connect Server
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
