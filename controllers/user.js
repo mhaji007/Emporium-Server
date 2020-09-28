7793fe5f0ad6350c8c0f27f6bcc63cbfbc6bd601
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-09-28 18:43:38 -0400

    Add custom error message for duplicate entries. Remove password and salt from API response

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const{errorHandler} = require("../helpers/dbErrorHandler")
 
 exports.signup = (req, res) => {
   console.log("req.body", req.body);
@@ -9,10 +10,13 @@ exports.signup = (req, res) => {
     if (err) {
       console.log(err);
       return res.status(400).json({
-        err
+        err: errorHandler(err)
       });
 
     }
+
+    user.salt = undefined;
+    user.hashed_password = undefined;
 
     res.json({
       user
