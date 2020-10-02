340b1b2f6dd472a977a54adbf7580f6a40fb0429
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-10-02 12:45:27 -0400

    Implement productById middleware

--- controllers/product.js
+++ controllers/product.js
@@ -6,6 +6,24 @@ const fs = require('fs');
 const Product = require("../models/product");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
+exports.productById = (req, res, next, id) => {
+  Product.findById(id).exec((err, product)=>{
+    if(err || !product) {
+      return res.status(400).json({
+        error: "Product not found"
+      });
+    }
+    req.product = product;
+    next();
+  });
+};
+
+
+exports.read = (req, res) => {
+  req.product.photo = undefined;
+  return res.json(req.product);
+}
+
 // We need to send the form data
 // to handle image upload
 exports.create = (req, res) => {
