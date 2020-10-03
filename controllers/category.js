ade785316f6c946819b12564d57eec3c72ad3322
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-10-03 19:04:30 -0400

    Implement update, delete and list all functionalities for category

--- controllers/category.js
+++ controllers/category.js
@@ -1,23 +1,26 @@
 const Category = require("../models/category");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
-
 exports.categoryById = (req, res, next, id) => {
-    Category.findById(id).exec((err, category) => {
-        if (err || !category) {
-            return res.status(400).json({
-                error: 'Category does not exist'
-            });
-        }
-        req.category = category;
-        next();
-    });
+  Category.findById(id).exec((err, category) => {
+    if (err || !category) {
+      return res.status(400).json({
+        error: "Category does not exist"
+      });
+    }
+    req.category = category;
+    next();
+  });
 };
 
 //==== Read category ====//
+
 exports.read = (req, res) => {
   return res.json(req.category);
 };
+
+//======================//
+
 //==== Create category ====//
 
 exports.create = (req, res) => {
@@ -25,12 +28,60 @@ exports.create = (req, res) => {
   category.save((err, data) => {
     if (err) {
       return res.status(400).json({
-        error: errorHandler(err)});
-      }
-      res.json({ data });
-    });
-  };
+        error: errorHandler(err)
+      });
+    }
+    res.json({ data });
+  });
+};
 
-//========================//
+//=========================//
 
+//==== Update category ====//
 
+exports.update = (req, res) => {
+  const category = req.category;
+  category.name = req.body.name;
+  category.save((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err)
+      });
+    }
+    res.json(data);
+  });
+};
+
+//=========================//
+
+//==== Remove category ====//
+
+exports.remove = (req, res) => {
+  const category = req.category;
+  category.name = req.body.name;
+  category.remove((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err)
+      });
+    }
+    res.json({ message: "Category deleted" });
+  });
+};
+
+//===========================//
+
+//==== Read All categories ====//
+
+exports.list = (req, res) => {
+  Category.find().exec((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err)
+      });
+    }
+    res.json(data);
+  });
+};
+
+//===========================//
--- routes/category.js
+++ routes/category.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, categoryById, read } = require("../controllers/category");
+const { create, categoryById, read, update, remove, list } = require("../controllers/category");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
 // CRUD routes
 router.get("/category/:categoryId", read);
 router.post("/category/create/:userId", requireSignin, isAuth, isAdmin, create);
+router.put("/category/:categoryId/:userId", requireSignin, isAuth, isAdmin, update);
+router.delete("/category/:categoryId/:userId", requireSignin, isAuth, isAdmin, remove);
+router.get("/categories", list);
 
 // Middlewares for routes in need
 // of userId and ProductId
-router.param("category", categoryById)
+router.param("categoryId", categoryById)
 router.param("userId", userById);
 
 module.exports = router;
