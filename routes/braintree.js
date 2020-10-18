9ab6f5055b9d30c3e3358c9ccae7712ba9cdb99c
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-10-18 13:19:50 -0400

    Add payment processing functionality

--- routes/braintree.js
+++ routes/braintree.js
@@ -3,11 +3,16 @@ const router = express.Router();
 
 const { requireSignin, isAuth } = require("../controllers/auth");
 const {userById} = require("../controllers/user");
-const {generateToken} = require("../controllers/braintree");
+const {generateToken, processPayment} = require("../controllers/braintree");
 
 // generateToken is a controller method for generating
 // a token when a request is sent to the below toure
 router.get('/braintree/getToken/:userId', requireSignin, isAuth, generateToken)
+
+router.post('/braintree/payment/:userId', requireSignin, isAuth, processPayment)
+
+
+
 
 router.param('userId', userById);
 
