757424e458dd7d5e465368667ca01847e2b1cf11
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-10-19 13:32:24 -0400

    Implement quantity update functionality

--- routes/order.js
+++ routes/order.js
@@ -4,11 +4,12 @@ const router = express.Router();
 const { requireSignin, isAuth} = require("../controllers/auth");
 const { userById, addOrderToUserHistory } = require("../controllers/user");
 const {create} = require("../controllers/order");
+const {decreaseQuantity} = require("../controllers/product");
 
 
 router.post(
     "/order/create/:userId",
-    requireSignin, isAuth,  addOrderToUserHistory, create
+    requireSignin, isAuth,  addOrderToUserHistory, decreaseQuantity, create
 );
 
 router.param("userId", userById)
