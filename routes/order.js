6819c75d83caba58c5e7332e83952903b59ea786
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-10-20 11:39:34 -0400

    Implement CRUD routes and controllers for orders

--- routes/order.js
+++ routes/order.js
@@ -1,17 +1,43 @@
 const express = require("express");
 const router = express.Router();
 
-const { requireSignin, isAuth} = require("../controllers/auth");
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById, addOrderToUserHistory } = require("../controllers/user");
-const {create} = require("../controllers/order");
-const {decreaseQuantity} = require("../controllers/product");
-
+const {
+    create,
+    listOrders,
+    getStatusValues,
+    orderById,
+    updateOrderStatus
+} = require("../controllers/order");
+const { decreaseQuantity } = require("../controllers/product");
 
 router.post(
     "/order/create/:userId",
-    requireSignin, isAuth,  addOrderToUserHistory, decreaseQuantity, create
+    requireSignin,
+    isAuth,
+    addOrderToUserHistory,
+    decreaseQuantity,
+    create
 );
 
-router.param("userId", userById)
+router.get("/order/list/:userId", requireSignin, isAuth, isAdmin, listOrders);
+router.get(
+    "/order/status-values/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    getStatusValues
+);
+router.put(
+    "/order/:orderId/status/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    updateOrderStatus
+);
+
+router.param("userId", userById);
+router.param("orderId", orderById);
 
 module.exports = router;
